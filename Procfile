421a587b011999c503f38c79b30d1007e5b6b25b
Omer <omer.rayah@gmail.com>
2017-07-26 19:37:30 -0700

    added files and changes required for deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn manabi.wsgi --log-file -
--- manabi/settings.py
+++ manabi/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'f4=k!k*a5n+9j^mlqw1f%zxze@3^h8y=cnt8d19(pkr=k5#(nf'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
@@ -122,3 +122,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
